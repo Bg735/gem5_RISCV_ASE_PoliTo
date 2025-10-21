aea6c0f8c8994c6ff7fc274f750b4146ff0f719d
Bg735 <f.lanzara04@gmail.com>
2025-10-21 16:03:05 +0200

    Update setup.bat to not start container on completion

--- setup.bat
+++ setup.bat
@@ -10,4 +10,4 @@ copy /b parte_*.tar %OUTPUT%
 
 docker rm -f gem5_polito 2>nul
 docker load -i gem5_polito_windows.tar
-docker run --name gem5_polito -it -v /run/desktop/mnt/host/wslg/.X11-unix:/tmp/.X11-unix -v /run/desktop/mnt/host/wslg:/mnt/wslg -e DISPLAY=:0 -e WAYLAND_DISPLAY=wayland-0 -e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir -e PULSE_SERVER=/mnt/wslg/PulseServer gem5_polito:windows
+docker create --name gem5_polito -it -v /run/desktop/mnt/host/wslg/.X11-unix:/tmp/.X11-unix -v /run/desktop/mnt/host/wslg:/mnt/wslg -e DISPLAY=:0 -e WAYLAND_DISPLAY=wayland-0 -e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir -e PULSE_SERVER=/mnt/wslg/PulseServer gem5_polito:windows
